6961a72cc66e92cf4640b3baa368c312217baf25
patryk406 <merph406@gmail.com>
2020-11-10 17:54:31 +0100

    Message
    Setting Nav, Home,Featured components,
     style and default cleaning.Setting Nav, Home,Featured components,
     style and default cleaning.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
-requestAnimationFrame("dotenv").config({
+require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My story about Journey`,
+    description: `We will go together to a fantastic journey, to the end of world and back ! get ready.`,
+    author: `@merphy`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,7 +18,9 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-google-fonts`,
-      options: [`baskerville\:400`, `muli\:300,700`],
+      options: {
+        fonts: [`baskerville\:400`, `muli\:300,700`],
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
@@ -31,15 +33,15 @@ module.exports = {
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+      // options: {
+      //   name: `gatsby-starter-default`,
+      //   short_name: `starter`,
+      //   start_url: `/`,
+      //   background_color: `#663399`,
+      //   theme_color: `#663399`,
+      //   display: `minimal-ui`,
+      //   icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      // },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
+import Nav from "../components/nav/index"
+import Featured from "../components/featured/index"
+import Home from "../components/home/index"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+
+// import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    {/* <SEO title="Home" /> */}
+    <Nav />
+    <Featured />
+    <Home/>
   </Layout>
 )
-
 export default IndexPage
