50f158783e53ab0283de546fa42872633ac9c0e0
patryk406 <merph406@gmail.com>
2020-11-10 18:16:26 +0100

    Creating footer, style.

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -29,29 +29,29 @@ export default () => (
             }
           }
         }
-      }
+      } 
     `}
     render={data => (
-     <div className="feed">
-         {data.allContentfulBlog.edges.map(edge=>(
-             <div key={edge.node.id} className='card' style={{
-                 backgroundImage: `linear-gradient(
+      <div className="feed">
+        {data.allContentfulBlog.edges.map(edge => (
+          <div key={edge.node.id} className='card' style={{
+            backgroundImage: `linear-gradient(
                     to bottom, 
                     rgba(10,10,10,0) 0%,
                     rgba(10,10,10,0) 50%,
                     rgba(10,10,10,0.7) 100%),
                     url(${edge.node.featuredImage.fluid.src})`
-             }}
-             
-             onClick={() => navigate(`/blog/${edge.node.slug}`)}
-             >
-                 {edge.node.category.map(category=>(
-                 <p className='card__category'>{category.title}</p>
-                 ))}
-                 <p className='card__title'>{edge.node.title}</p>
-             </div>
-    ))}
-     </div>
+          }}
+
+            onClick={() => navigate(`/blog/${edge.node.slug}`)}
+          >
+            {edge.node.category.map(category => (
+              <p className='card__category' key={category.id}>{category.title}</p>
+            ))}
+            <p className='card__title'>{edge.node.title}</p>
+          </div>
+        ))}
+      </div>
     )}
   />
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from 'gatsby'
 import Nav from "../components/nav/index"
 import Featured from "../components/featured/index"
 import Home from "../components/home/index"
+import Footer from "../components/footer/index"
 
 import Layout from "../components/layout"
-
+import './index.css'
 // import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -12,7 +14,9 @@ const IndexPage = () => (
     {/* <SEO title="Home" /> */}
     <Nav />
     <Featured />
-    <Home/>
+    <Home />
+    <Link to='/blog' className='viewmore'>View More</Link>
+    <Footer />
   </Layout>
 )
 export default IndexPage
